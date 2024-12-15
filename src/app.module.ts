9bddc6ba211e636531960b90e40a302f4a28bcf3
tanvirshaad <tanvirshaad@gmail.com>
2024-12-15 22:23:31 +0600

    db added

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,22 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [UsersModule, PostsModule, AuthModule, TypeOrmModule.forRoot(
+    {
+      type: 'postgres',
+      entities: [],
+      synchronize: true,
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'nestjs-blog',
+      host: 'localhost',
+    }
+  )],
   controllers: [AppController],
   providers: [AppService],
 })
